cdaea667efad2e05f64fc348443e8ee96765f3cd
zyfone <478756030@qq.com>
2026-06-20 16:00:46 +0800

    initialization

--- mmseg/models/decode_heads/euler_margin.py
+++ mmseg/models/decode_heads/euler_margin.py
@@ -53,7 +53,7 @@ class EulerFormer(nn.Module):
         self.hidden_size = hidden_size
         self.vector_wise = vector_wise
         self.tau = tau 
-        self.delta = nn.Parameter(torch.ones(1,1,self.hidden_size//2)*0.1)
+        self.delta = nn.Parameter(torch.ones(1,1,self.hidden_size//2))
         self.b = nn.Parameter(torch.zeros(1,1,self.hidden_size//2))
         self.log_scale = nn.Parameter(torch.zeros(1,1,self.hidden_size//2))
         self.soft_sort=NeuralSort()
